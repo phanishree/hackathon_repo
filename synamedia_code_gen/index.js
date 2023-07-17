71ef1593ef06961cde1516501ea1f4ea6784abec
Phanishree K <pk@synamedia.com>
2023-07-17 21:14:07 +0530

    Added basic api doc inside .docx file and added a prompt to generate yaml format

--- synamedia_code_gen/index.js
+++ synamedia_code_gen/index.js
@@ -1,15 +1,16 @@
-const {Configuration, OpenAIApi} = require("openai");
+const { Configuration, OpenAIApi } = require("openai");
 require("dotenv").config();
 
 const configuration = new Configuration({
-    apiKey : process.env.api_key_for_hackathon,
+    apiKey: process.env.API_KEY,
 });
 
-const openai= new OpenAIApi(configuration);
+const openai = new OpenAIApi(configuration);
 
-const prompt="Hey, how are you doing today?";
+const prompt =`Generate a proper yaml file for the below API documentation
 
-async function runGpt (){
+"Parameters lat, lon required Geographical coordinates (latitude, longitude). If you need the geocoder to automatic convert city names and zip-codes to geo coordinates and the other way around, please use our Geocoding API. appid required Your unique API key (you can always find it on your account page under the API key tab) mode optional Response format. Possible values are xml and html. If you don't use the mode parameter format is JSON by default. Learn more units optional Units of measurement. standard, metric and imperial units are available. If you do not use the units parameter, standard units will be applied by default. Learn more lang optional You can use this parameter to get the output in your language."`
+async function runGpt() {
     const generatedReply = await openai.createCompletion({
         model: "text-davinci-003",
         prompt: prompt,
--- synamedia_code_gen/parseFile.js
+++ synamedia_code_gen/parseFile.js
@@ -1,7 +1,7 @@
 var textract = require('textract');
 
 
-var filePath= "./public/assets/docx/Panishree 301.docx";        
+var filePath= "./public/assets/docx/basic-api-doc.docx";        
 textract.fromFileWithPath(filePath, function( error, text ) {
     console.log(text);
 })
