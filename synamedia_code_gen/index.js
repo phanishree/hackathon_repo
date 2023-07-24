4f439e70ff7ed9b536966b4756748cbb3454040f
Ganesh Arumugam <garumugam@synamedia.com>
2023-07-24 14:51:16 +0530

    go work

--- synamedia_code_gen/index.js
+++ synamedia_code_gen/index.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 const path = require('path');
-const { yaml_prompt_1, doc_prompt, direct_promtpt, final_prompt } = require('./prompts');
+const { yaml_prompt_1, doc_prompt, direct_promtpt, final_prompt: common_prompt } = require('./prompts');
 const testCodeGenerator = require('./testGenerator')
 const helper = require('./helper')
 const openai = require('./apiRouter')
@@ -9,16 +9,16 @@ const folderPath = path.join('/Users/garumugam/Desktop', 'Syna_API');
 
 async function start() {
   try {
-    // TO-DO: Phani carry on :)
-    // const prompt = documentExtractor() + doc_prompt
+    const api_doc_text = await documentExtractor()
+    const prompt =  common_prompt + api_doc_text
 
-    const response = await openai.runGpt(final_prompt);
+    const response = await openai.runGpt(prompt);
     if (!fs.existsSync(folderPath)) {
       fs.mkdirSync(folderPath);
     }
     const serverFilePath = path.join(folderPath, 'server.js');
     await helper.parsingResponse(response, serverFilePath);
-    // await testCodeGenerator.main(serverFilePath);
+    await testCodeGenerator.main(serverFilePath);
   } catch (error) {
     console.error("Error - Starting the process:", error.message);
   }
