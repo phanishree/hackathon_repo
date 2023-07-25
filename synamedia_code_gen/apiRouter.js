a3706b5fbeba378301a8cb983e1e98557d05fa1a
Phanishree K <pk@synamedia.com>
2023-07-25 12:29:57 +0530

    Added document which has 10 routes

--- synamedia_code_gen/apiRouter.js
+++ synamedia_code_gen/apiRouter.js
@@ -10,37 +10,37 @@ const configuration = new Configuration({
 const openai = new OpenAIApi(configuration);
 
 async function runGpt(prompt) {
-    // try {
-    //     const response = await axios.post(
-    //         "https://api.openai.com/v1/chat/completions",
-    //       {
-    //         model: "gpt-3.5-turbo-16k",
-    //         temperature: 0,
-    //         messages: [
-    //             {
-    //                 role: "user",
-    //                 content: prompt
-    //             },
-    //         ],
-    //     },
-    //      {
-    //         headers: {
-    //             "Content-Type": "application/json",
-    //             Authorization: "Bearer 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",
-    //         },
-    //      }
-    //      );
-    //      return response
-    //     }
-  try {
-    const response = await openai.createCompletion({
-      model: "text-davinci-003", // Change model gpt-3.5-turbo-16k to use 16000 tokens
-      prompt: prompt,
-      temperature: 0,
-      max_tokens: 1000, 
-    });
-    return response;
-  }
+    try {
+        const response = await axios.post(
+            "https://api.openai.com/v1/chat/completions",
+          {
+            model: "gpt-3.5-turbo-16k",
+            temperature: 0,
+            messages: [
+                {
+                    role: "user",
+                    content: prompt
+                },
+            ],
+        },
+         {
+            headers: {
+                "Content-Type": "application/json",
+                Authorization: "Bearer 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",
+            },
+         }
+         );
+         return response
+        }
+  // try {
+  //   const response = await openai.createCompletion({
+  //     model: "text-davinci-003", // Change model gpt-3.5-turbo-16k to use 16000 tokens
+  //     prompt: prompt,
+  //     temperature: 0,
+  //     max_tokens: 1000, 
+  //   });
+  //   return response;
+  // }
    catch (error) {
     console.error("Error - In Api call", error.message);
     throw error; // Rethrow the error to handle it elsewhere if needed
